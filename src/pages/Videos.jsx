be2061577795ac2505f8bb392ea43ca849cd90ce
wBeeDev <hiryuji@kakao.com>
2023-09-20 20:05:43 +0900

    Context

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -4,17 +4,16 @@ import { useQuery } from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
 import Youtube from "../api/youtube";
 import FakeYoutube from "../api/fakeYoutube";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new Youtube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
 
   return (
     <div>
